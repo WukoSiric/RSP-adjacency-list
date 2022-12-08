9cf12d1dd8a7939ebda764edda065480f5447783
WukoSiric <lstainer2002@gmail.com>
2022-12-08 15:32:54 +1030

    Undirected graph adj list made

--- adj-list.cpp
+++ adj-list.cpp
@@ -1,8 +1,42 @@
 #include <iostream> 
-#include <vector> 
+#include <list> 
+#include <vector>
+// Undirected graph using adjacency list
 
+class Graph {
+    int num_vertices;
+    std::vector<std::list<int>> adj_list;
+public: 
+    std::vector<std::list<int>> * get_adj_list() {
+        return &(this->adj_list);
+    }
 
+    Graph(int v) {
+        this->num_vertices = v;
+        this->adj_list.resize(v);
+    } 
+    void add_edge(int src, int dest) {
+        this->adj_list[src].push_back(dest);
+        this->adj_list[dest].push_back(src);
+    }
+};
 
 int main() {
+    Graph g(5);
+    g.add_edge(0, 1);
+    g.add_edge(0, 4);
+    g.add_edge(1, 2);
+    g.add_edge(1, 3);
+    g.add_edge(1, 4);
+
+    // Get adj list 
+    std::vector<std::list<int>> * adj_list = g.get_adj_list();
+    for (int i = 0; i < adj_list->size(); i++) {
+        std::cout << "VERTEX " << i << ":";
+        for (auto x : (*adj_list)[i]) {
+            std::cout << " -> " << x;
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
