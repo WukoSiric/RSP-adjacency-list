e81574c1394705e9b384cbc6ca978b6a6f2383d5
WukoSiric <lstainer2002@gmail.com>
2022-12-10 15:24:13 +1030

    Refactored main to printing function

--- adj-list.cpp
+++ adj-list.cpp
@@ -2,7 +2,6 @@
 #include <list> 
 #include <vector>
 // Undirected graph using adjacency list
-
 class Graph {
     int num_vertices;
     std::vector<std::list<int>> adj_list;
@@ -14,14 +13,28 @@ public:
     Graph(int v) {
         this->num_vertices = v;
         this->adj_list.resize(v);
-    } 
+    }
+
     void add_edge(int src, int dest) {
         this->adj_list[src].push_back(dest);
         this->adj_list[dest].push_back(src);
     }
 };
 
+// Print Adj List Function
+void print_graph(std::vector<std::list<int>> * adj_list) {
+    for (int i = 0; i < adj_list->size(); i++) {
+        std::cout << "VERTEX " << i << ":";
+        for (auto x : (*adj_list)[i]) {
+            std::cout << " -> " << x;
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main() {
+    std::cout << "Welcome to my graph program!" << std::endl;
+    std::cout << "Creates an undirected graph, has BFS so far..." << std::endl;
     Graph g(5);
     g.add_edge(0, 1);
     g.add_edge(0, 4);
@@ -30,13 +43,6 @@ int main() {
     g.add_edge(1, 4);
 
     // Get adj list 
-    std::vector<std::list<int>> * adj_list = g.get_adj_list();
-    for (int i = 0; i < adj_list->size(); i++) {
-        std::cout << "VERTEX " << i << ":";
-        for (auto x : (*adj_list)[i]) {
-            std::cout << " -> " << x;
-        }
-        std::cout << std::endl;
-    }
+    print_graph(g.get_adj_list());
     return 0;
 }
